d8a6210d3c52639cd6d86bc19f48215815e00dea
zhupeng <253908058@qq.com>
2019-06-10 15:58:01 +0800

    消息

---
+++ app/src/main/java/com/swpu/funchat/ui/home/adapter/MessageAdapter.java
@@ -0,0 +1,38 @@
+package com.swpu.funchat.ui.home.adapter;
+
+import androidx.annotation.Nullable;
+
+import com.swpu.funchat.base.RecyclerAdapter;
+import com.swpu.funchat.base.RecyclerViewHolder;
+import com.swpu.funchat.model.MessageEntity;
+
+import java.util.List;
+
+/**
+ * Class description:
+ *
+ * @author zp
+ * @version 1.0
+ * @see MessageAdapter
+ * @since 2019-06-06
+ */
+public class MessageAdapter extends RecyclerAdapter<MessageEntity> {
+    /**
+     * the constructor of this class.
+     *
+     * @param items the data source.
+     */
+    public MessageAdapter(@Nullable List<MessageEntity> items) {
+        super(items);
+    }
+
+    @Override
+    protected int getLayoutByViewType(int viewType) {
+        return 0;
+    }
+
+    @Override
+    protected void onBindViewHolder(RecyclerViewHolder holder, int position, MessageEntity item) {
+
+    }
+}
---
+++ app/src/main/java/com/swpu/funchat/vm/MessageViewModel.java
@@ -0,0 +1,30 @@
+package com.swpu.funchat.vm;
+
+import androidx.lifecycle.MediatorLiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModel;
+
+import com.swpu.funchat.model.MessageEntity;
+
+/**
+ * Class description:
+ *
+ * @author zp
+ * @version 1.0
+ * @see MessageViewModel
+ * @since 2019-06-06
+ */
+public class MessageViewModel extends ViewModel {
+
+    private MutableLiveData<MessageEntity> mLiveData;
+    private MediatorLiveData<MessageEntity> mMessageLiveData;
+
+    public MessageViewModel() {
+        mMessageLiveData = new MediatorLiveData<>();
+        mLiveData = new MutableLiveData<>();
+        mMessageLiveData.setValue(null);
+
+        mMessageLiveData.addSource(mLiveData, mLiveData::setValue);
+    }
+}
